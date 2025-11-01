a7ea731f5e014141c4816f99bb51b2a0382f82e3
GoldBro233 <bowensun_06@outlook.com>
2025-11-02 00:19:31 +0800

    Fix: Change db Auto Migrate Position to InitDB()

--- config/db.go
+++ config/db.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"chronote/global"
+	"chronote/models"
 
 	"fmt"
 	"log"
@@ -15,13 +16,20 @@ func buildDSN(user, password, host, port, dbname, sslmode, TimeZone string) stri
 	return fmt.Sprintf(template, host, user, password, dbname, port, sslmode, TimeZone)
 }
 
-func initDB() {
+func InitDB() {
 	dsn := buildDSN(AppConfig.Database.Host, AppConfig.Database.User, AppConfig.Database.Password, AppConfig.Database.dbName, AppConfig.Database.Port, AppConfig.Database.sslmode, AppConfig.Database.TimeZone)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalf("Failed to connect to database,%v", err)
 	}
+	err = db.AutoMigrate(
+		&models.User{},
+	)
+	if err != nil {
+		log.Fatalf("Failed to auto migrate database, %v", err)
+	}
+	log.Println("Database connected and migrated successfully.")
 
 	global.Db = db
 }
