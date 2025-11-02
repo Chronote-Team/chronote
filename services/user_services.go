becc31cf9d21bbc875c21c9c40c6ba16919083fa
GoldBro233 <bowensun_06@outlook.com>
2025-11-02 18:25:20 +0800

    Add: User Login Service.

--- services/user_services.go
+++ services/user_services.go
@@ -1,9 +1,11 @@
 package services
 
 import (
+	"chronote/config"
 	"chronote/global"
 	"chronote/models"
 	"chronote/utils"
+	"errors"
 )
 
 type UserService struct{}
@@ -18,4 +20,38 @@ func (s *UserService) Register(user *models.User) error {
 		return err
 	}
 	return nil
+}
+
+type LoginResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn    int64  `json:"expires_in"`
+}
+
+func (s *UserService) Login(email, password string) (*LoginResponse, error) {
+	var user models.User
+
+	// Verify User Email and Password
+	if err := global.Db.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, errors.New("用户不存在或密码错误")
+	}
+	passwordMatch, err := utils.VerifyPassword(password, user.Password)
+	if err != nil || !passwordMatch {
+		return nil, errors.New("用户不存在或密码错误")
+	}
+
+	// Generate Tokens
+	accessToken, refreshToken, err := utils.GenerateToken(user.ID, user.Username)
+	if err != nil {
+		return nil, errors.New("Token 生成失败")
+	}
+	expiresInSeconds := config.AppConfig.JWT.AccessTokenExpire
+
+	// Generate Login Response
+	loginResponse := &LoginResponse{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+		ExpiresIn:    expiresInSeconds,
+	}
+	return loginResponse, nil
 }
